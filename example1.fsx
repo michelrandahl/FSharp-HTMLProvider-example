0d527fccce5be01b75c0a17b40269fc47744f779
Michel Bøje Randahl Nielsen <michel@randahl.net>
2016-07-03 13:48:25 +0200

    Update example1.fsx

--- example1.fsx
+++ example1.fsx
@@ -9,18 +9,20 @@ type HTML = HtmlProvider<"http://fsharp.github.io/FSharp.Data/index.html">
 
 let html = HTML.GetSample()
 
-html.Lists.Html.CssSelect "li"
-|> Seq.skipWhile (fun el -> el.InnerText() <> "Documentation")
-|> Seq.skip 3
-|> Seq.takeWhile (fun el -> Seq.length(el.CssSelect "a") > 0)
-|> Seq.map (fun el -> el.CssSelect "a" |> Seq.head)
-|> Seq.map (fun el -> el.Attribute "href")
-|> Seq.map (fun el -> el.Value())
-|> Seq.map ((+) base_url)
-|> Seq.map (HTML.AsyncLoad) // using the same type because all these pages look the same
-|> Async.Parallel
-|> Async.RunSynchronously
-|> Seq.map (fun site -> site.Html.CssSelect "h1" |> Seq.head)
-|> Seq.map (fun el -> el.InnerText())
-|> List.ofSeq
+let type_provider_articles_titles =
+    html.Lists.Html.CssSelect "li"
+    |> Seq.skipWhile (fun el -> el.InnerText() <> "Documentation")
+    |> Seq.skip 3
+    |> Seq.takeWhile (fun el -> Seq.length(el.CssSelect "a") > 0)
+    |> Seq.map (fun el -> el.CssSelect "a" |> Seq.head)
+    |> Seq.map (fun el -> el.Attribute "href")
+    |> Seq.map (fun el -> el.Value())
+    |> Seq.map ((+) base_url)
+    |> Seq.map (HTML.AsyncLoad) // using the same type because all these pages look the same
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.map (fun site -> site.Html.CssSelect "h1" |> Seq.head)
+    |> Seq.map (fun el -> el.InnerText())
+    |> List.ofSeq
+type_provider_articles_titles
 |> printfn "%A"
