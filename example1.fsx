e4f765e720b76fa0b269603dc7a88657cba348bc
Michel Bøje Randahl Nielsen <michel@randahl.net>
2016-07-03 13:35:32 +0200

    Update example1.fsx

--- example1.fsx
+++ example1.fsx
@@ -1,14 +1,15 @@
 #r "../../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "System.Xml.Linq.dll"
+
 open FSharp.Data
 
 let base_url = "http://fsharp.github.io"
 
-type HTML_FTP = HtmlProvider<"http://fsharp.github.io/FSharp.Data/index.html">
+type HTML = HtmlProvider<"http://fsharp.github.io/FSharp.Data/index.html">
 
-let ftp = HTML_FTP.GetSample()
+let html = HTML.GetSample()
 
-ftp.Lists.Html.CssSelect "li"
+html.Lists.Html.CssSelect "li"
 |> Seq.skipWhile (fun el -> el.InnerText() <> "Documentation")
 |> Seq.skip 3
 |> Seq.takeWhile (fun el -> Seq.length(el.CssSelect "a") > 0)
@@ -16,7 +17,7 @@ ftp.Lists.Html.CssSelect "li"
 |> Seq.map (fun el -> el.Attribute "href")
 |> Seq.map (fun el -> el.Value())
 |> Seq.map ((+) base_url)
-|> Seq.map (HTML_FTP.AsyncLoad)
+|> Seq.map (HTML.AsyncLoad) // using the same type because all these pages look the same
 |> Async.Parallel
 |> Async.RunSynchronously
 |> Seq.map (fun site -> site.Html.CssSelect "h1" |> Seq.head)
